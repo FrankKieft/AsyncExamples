da5a7db72b51c79f57073f549b083c413353ed2c
FrankKieft <frank.kieft@live.nl>
2019-10-27 10:03:44 +0100

    Finished calculators.

--- Domain/Calculators/HashFactorCalculator.cs
+++ Domain/Calculators/HashFactorCalculator.cs
@@ -1,4 +1,8 @@
 ﻿using AsyncExamples.Domain.Utils;
+using System;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading;
 
 namespace AsyncExamples.Domain.Calculators
 {
@@ -6,7 +10,37 @@ namespace AsyncExamples.Domain.Calculators
     {
         public override void Calculate(int x, CalculationLog log)
         {
-            throw new System.NotImplementedException();
+            var t = DateTime.Now;
+            var y = 0;
+
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                while (!IsFactor(x, ++y, sha256Hash))
+                {
+
+                }
+            }
+
+            log.Items.Add(new CalculationLogItem
+            {
+                Result = y,
+                Name = GetType().Name,
+                MilliSeconds = (DateTime.Now - t).TotalMilliseconds,
+                Thread = Thread.CurrentThread.Name
+            });
+        }
+
+        private bool IsFactor(int x, int y, SHA256 sha256Hash)
+        {
+            byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(x.ToString() + y.ToString()));
+
+            StringBuilder builder = new StringBuilder();
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                builder.Append(bytes[i].ToString("x2"));
+            }
+
+            return builder.ToString().EndsWith("0000");
         }
     }
 }
---
+++ DomainTests/HashcodeFactorCalculationTests.cs
@@ -0,0 +1,26 @@
+﻿using AsyncExamples.Domain.Calculators;
+using AsyncExamples.Domain.Utils;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace AsyncExamples.DomainTests
+{
+    [TestClass]
+    public class HashcodeFactorCalculationTests
+    {
+        [TestMethod]
+        public void TheHashcodeFactorOf25Is1234()
+        {
+            // Arrange
+            var calculator = new HashFactorCalculator();
+            var log = new CalculationLog();
+
+            // Act
+            calculator.Calculate(25, log);
+
+            // Assert
+            log.Items.Single().Result.Should().Be(13638);
+        }
+    }
+}
